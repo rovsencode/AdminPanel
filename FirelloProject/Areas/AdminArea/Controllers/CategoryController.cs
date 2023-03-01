028217f668383a0640e9f3ed15be8c80edbb04d8
rovsencode <rovshanakh@code.edu.az>
2023-03-01 18:27:16 +0400

    Add Admin Panel

---
+++ FirelloProject/Areas/AdminArea/Controllers/CategoryController.cs
@@ -0,0 +1,29 @@
+﻿using FirelloProject.DAL;
+using FirelloProject.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirelloProject.Areas.AdminArea.Controllers
+{
+    [Area("AdminArea")]
+    public class CategoryController : Controller
+    {
+        private readonly AppDbContext _appDbContext;
+
+        public CategoryController(AppDbContext appDbContext)
+        {
+            _appDbContext = appDbContext;
+        }
+
+        public IActionResult Index()
+        {
+            return View(_appDbContext.Products.ToList());
+        }
+
+        public IActionResult Detail(int id)
+        {
+            Category category=_appDbContext.Categories.SingleOrDefault(c=>c.ID==id);
+            if (category == null) return NotFound();
+            return View(category);
+        }
+    }
+}
---
+++ FirelloProject/Models/Category.cs
@@ -0,0 +1,11 @@
+﻿namespace FirelloProject.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; }
+        public List<Product>? Products { get; set; }
+
+    }
+}
